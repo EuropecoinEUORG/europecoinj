225679d8e2427cfda0a967904570d88ef37c37ff
madzera <madzebra@users.noreply.github.com>
2017-03-05 17:42:23 +0000

    reverted min proto

--- core/src/main/java/org/bitcoinj/core/NetworkParameters.java
+++ core/src/main/java/org/bitcoinj/core/NetworkParameters.java
@@ -519,7 +519,7 @@ public abstract class NetworkParameters {
     public abstract int getProtocolVersionNum(final ProtocolVersion version);
 
     public static enum ProtocolVersion {
-        MINIMUM(70002),
+        MINIMUM(70003),
         PONG(60001),
         BLOOM_FILTER(70000),
         CURRENT(70003);
